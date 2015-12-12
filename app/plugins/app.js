b13b9502d917a5458bc5bb17fcd53ba54bf90082
LORDofDOOM <tb@bbz-int.de>
2015-12-12 06:19:05 +0100

    [Core] Add Routing

---
+++ app/plugins/app.js
@@ -0,0 +1,10 @@
+/**
+ * CybeSystems Angular Admin Template
+ *
+ */
+(function () {
+    angular.module('cyadmin', [
+        'ui.router',                    // Routing
+        'ui.bootstrap'                 // Bootstrap
+    ])
+})();
